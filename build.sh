04a9f2861fcb6129a18906b93fcb16861b06aef5
Albert Strasheim <albert.strasheim@vastech.co.za>
2013-03-31 23:19:31 +0200

    closer to linking, but issues remain.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ gofmt -s -w *.go
 go run gen.go
 gofmt -s -w *.go
 go tool 6g -I pkg main.go
-go tool 6l -L pkg -L pkg/math/_test -L pkg/sync/_test -L pkg/net/http/_test/net -L pkg/strings/_test main.6
+go tool 6l -L pkg main.6
--- prepare.sh
+++ prepare.sh
@@ -8,3 +8,5 @@ mkdir -p pkg
 cp -a $TMPDIR/*/* pkg
 rm -rf $TMPDIR
 find $GOROOT/src -name testdata -type d | xargs cp -at .
+find pkg -type l -name '*.a' -print0 | xargs -0 rm -f
+find `pwd`/pkg -name '*.a' -a ! -name '*_test.a' -a ! -path '*/_test/main.a' | perl -ne 'chomp;$p=$_;s/.*\/_test\///;`ln -sf $p pkg/$_`;'
